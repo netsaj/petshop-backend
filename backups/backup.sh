d05cde0209774469b577c1f7c4a34714d900bc59
Fabio Moreno <fabiomoreno@outlook.com>
2020-11-05 05:49:19 -0500

    fix: solved error in bat file

--- backups/backup.sh
+++ backups/backup.sh
@@ -1,4 +1,4 @@
-DATE=`date '+%Y%m%d%H%M%S'`
+DATE=`date '+%Y%m%d% H_%M_%S'`
 export PGPASSWORD=linux
 pg_dump --file  "./backups/${DATE}-petshop.backup"  --host "localhost" --port "5432" --username "postgres" --no-password \
   --verbose --format=c --blobs --section=pre-data --section=data --section=post-data "petshop"
